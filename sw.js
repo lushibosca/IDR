203ab368a98ddd2b3643b8b3518f107275d0d90e
luis <laguirre@hcdn.gob.ar>
2026-09-14 17:36:37 -0300

    SGC se colocan los valores de canales de grabacion pegados hacia abajo de cada item

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
  * Control centralizado para Launcher y submódulos (SGC, SGI, SGL, SGR)
  */
 
-const CACHE_NAME = 'idr-suite-260911.1815';
-const RUNTIME_CACHE = 'idr-runtime-260911.1815';
+const CACHE_NAME = 'idr-suite-260914.1740';
+const RUNTIME_CACHE = 'idr-runtime-260914.1740';
 
 // Recursos esenciales que se precachean para garantizar funcionamiento offline total
 const PRECACHE_URLS = [
